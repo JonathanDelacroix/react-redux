9ba06aa6a50d3274b6963b1f79a3f3bc41aacebe
delacroix <delacroix.jon@gmail.com>
2025-06-25 17:01:55 +0200

    Activité 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, toggleTodo } from "./redux/todoSlice";
+import { selectTodos, selectCompletedTodos } from "./redux/selectors";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector(selectTodos);
+  const completedTodos = useSelector(selectCompletedTodos);
+  const handleAddTodo = () => {
+    dispatch(addTodo(input));
+    setInput("");
+  };
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  <div>
+    <h1>Liste des tâches</h1>
+    <input
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      placeholder="Ajouter une tâche..."
+    />
+    <button onClick={handleAddTodo}>Ajouter</button>
+    <ul>
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          onClick={() => dispatch(toggleTodo(todo.id))}
+          >
+          {todo.text}
+        </li>
+      ))}
+    </ul>
+    <h2>Tâches terminées</h2>
+    <ul>
+      {completedTodos.map((todo) => (
+      <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  </div>
+  );
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
+import React, { StrictMode } from 'react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+<StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+</StrictMode>
+);
